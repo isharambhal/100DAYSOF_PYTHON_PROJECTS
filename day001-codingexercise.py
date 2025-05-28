6b4048e3e1e0ac42c9d6025bed1298d87c65b23e
Isha Rambhal <146533563+isharambhal@users.noreply.github.com>
2025-05-29 03:53:28 +0530

    Add files via upload

---
+++ day001-codingexercise.py
@@ -0,0 +1,12 @@
+#we have 2 variables, glass1 & glass2 where glass1 contains
+# milk and glass2 contains juice. Write 3 lines of code to switch
+# the content of the variables
+
+glass1 = "milk"
+glass2 = "juice"
+
+# Switching the content by adding extra variable
+empty = glass1
+glass1 = glass2
+glass2 = glass1
+print(glass1)
