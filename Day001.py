aa79a86a07d7a7183fb71f5a9a0e29eeacbc5120
Isha Rambhal <146533563+isharambhal@users.noreply.github.com>
2025-05-29 03:41:54 +0530

    Add files via upload

---
+++ Day001.py
@@ -0,0 +1,10 @@
+#Band Name Generator
+# Step1- Create Greeting
+# Step2- Ask the user's favorite destination
+# Step3- Ask the user for the name of a pet
+# Step4- Combine the name of fav destination and pet's name(Concatenation)
+
+print("Welcome to the Band Name Generator!")
+fav_city = input("Which city you love the most? \n")
+pet_name = input("Name of your pet please! \n")
+print("Your Band Name could be : " + fav_city + " " + pet_name)
