7bc1dc871e04272a2f860bc0af7c212b0b01ac2b
Isha Rambhal <146533563+isharambhal@users.noreply.github.com>
2025-05-31 23:49:49 +0530

    Add files via upload

---
+++ Day003.py
@@ -0,0 +1,59 @@
+print(r'''
+ _                                     
+| |                                    
+| |_ _ __ ___  __ _ ___ _   _ _ __ ___ 
+| __| '__/ _ \/ _` / __| | | | '__/ _ \
+| |_| | |  __/ (_| \__ \ |_| | | |  __/
+ \__|_|  \___|\__,_|___/\__,_|_|  \___|
+
+
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+print("let's start the game!")
+user_choice1 = input('where do you want to go? '
+                     'Type "left" or "right".\n').lower()
+
+if user_choice1 == "left":
+    user_choice2 = input('You\'ve come to a lake. '
+                         'There is an island in the middle of the lake. '
+                         'Type "wait" to wait for a boat. '
+                         'Type "swim" to swim across.\n').lower()
+    if user_choice2 == "wait":
+        user_choice3 = input("You arrive at the island unharmed. "
+                             "There is house with 3 doors. One red, "
+                             "one yellow and one blue. "
+                             "Which colour do you choose?\n").lower()
+        if user_choice3 == "red":
+            print("It's a room full of fire. Game Over")
+        elif user_choice3 == "yellow":
+            print("You found the treasure. You Win!")
+        elif user_choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You got attacked by an angry trout. Game Over.")
+
+else:
+    print("You fell in to a hole. Game Over.")
